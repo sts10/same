d3e6ea00c267a04762919fd02041a9d0327110f7
Sam Schlinkert <sschlinkert@gmail.com>
2021-10-29 17:55:54 -0400

    adds tests

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     println!("Making first hash");
     let hash1 = hash_dir(test_path);
     println!("Making second hash");
-    let hash2 = hash_dir("/home/sschlinkert/code");
+    let hash2 = hash_dir("/home/sschlinkert/Documents");
 
     if hash1 == hash2 {
         println!("Matched!");
@@ -19,10 +19,33 @@ fn hash_dir(dir_path: &str) -> blake3::Hash {
     let mut hasher = blake3::Hasher::new();
 
     for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
-        if entry.metadata().unwrap().is_file() {
+        if entry.metadata().unwrap().is_file()
+            && entry
+                .path()
+                .starts_with("/home/sschlinkert/.steam/steam.pipe")
+                == false
+        {
             let mut file = fs::File::open(&entry.path()).expect("Error opening a file for hashing");
             let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
         }
     }
     hasher.finalize()
+}
+
+#[cfg(test)]
+mod basic_tests {
+    use super::*;
+
+    #[test]
+    fn can_determine_same_directory_is_same() {
+        let path = "/home/sschlinkert/code/tic-tac-go";
+        assert_eq!(hash_dir(path), hash_dir(path));
+    }
+
+    #[test]
+    fn can_determine_different_directories_are_different() {
+        let path1 = "/home/sschlinkert/code/tic-tac-go";
+        let path2 = "/home/sschlinkert/code/tidy";
+        assert_ne!(hash_dir(path1), hash_dir(path2));
+    }
 }

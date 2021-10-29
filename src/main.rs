0fe4e3021e9a69b38595cc0f30ac60017f57ecd0
Sam Schlinkert <sschlinkert@gmail.com>
2021-10-29 17:46:38 -0400

    safe

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-use std::path::Path;
 use std::{fs, io};
 use walkdir::WalkDir;
 
 fn main() {
-    // let test_path = "/media/sschlinkert/My Book/back-ups-rsync-snapshot-feb-2021/Documents/macbook-air-code/buzzfeed";
-    // let test_path =
-    //     "/media/sschlinkert/My Book/back-ups-rsync-snapshot-feb-2021/Documents/macbook-air-code";
-    let test_path = "home/sschlinkert/code";
-    println!("path is {}", test_path);
-
+    let test_path = "/home/sschlinkert/code";
     println!("Making first hash");
-    let hash_vec1 = hash_dir(test_path);
+    let hash1 = hash_dir(test_path);
     println!("Making second hash");
-    let hash_vec2 = hash_dir(test_path);
+    let hash2 = hash_dir("/home/sschlinkert/code");
 
-    println!("{:?}\nvs\n{:?}", hash_vec1, hash_vec2);
-    if hash_vec1 == hash_vec2 {
+    if hash1 == hash2 {
         println!("Matched!");
     } else {
         println!("Does not match");
@@ -24,17 +17,9 @@ fn main() {
 
 fn hash_dir(dir_path: &str) -> blake3::Hash {
     let mut hasher = blake3::Hasher::new();
-    let exclude_path = Path::new("/home/sschlinkert/.steam/steam.pipe");
 
     for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
-        // println!("{:?}", entry.path());
-        if entry.metadata().unwrap().is_file()
-            // && entry
-            //     .path()
-            //     .starts_with("/home/sschlinkert/.steam/steam.pipe")
-            //     == false
-            && entry.path() != exclude_path
-        {
+        if entry.metadata().unwrap().is_file() {
             let mut file = fs::File::open(&entry.path()).expect("Error opening a file for hashing");
             let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
         }

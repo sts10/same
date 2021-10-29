85345b06d5380785f166c7a03f3ed2278db69c7c
Sam Schlinkert <sschlinkert@gmail.com>
2021-10-29 16:01:08 -0400

    does whole directory in a single hasher, rather than a Vector. This may be slower, but it's more ergonomic

---
+++ scrap_code.markdown
@@ -0,0 +1,42 @@
+
+```rust
+fn _hash(path: &Path, mut hasher: blake3::Hasher) -> String {
+    let mut file = fs::File::open(&path).expect("Error opening a file for hashing");
+    // let mut hasher = Blake2b::new();
+    let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
+    let hash = hasher.finalize();
+    // println!("Path: {}", path);
+    // println!("Bytes processed: {}", n);
+    // println!("Hash value: {:?}", hash);
+    return hash.to_string();
+}
+
+fn _add_to_hash(path: &Path, mut hasher: blake3::Hasher) {
+    let mut file = fs::File::open(&path).expect("Error opening a file for hashing");
+    // let mut hasher = Blake2b::new();
+    let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
+}
+```
+
+```rust
+
+    // let hash1 = blake3::hash(b"test");
+    // println!("Found hash: {}", hash1);
+
+    // let path = "foo.txt";
+    // let hasher = blake3::Hasher::new();
+    // let hash1 = hash(&path, hasher.clone());
+    // // let mut hasher = blake3::Hasher::new();
+    // let hash2 = hash(&path, hasher.clone());
+    // println!("{}\nvs\n{}", hash1, hash2);
+
+    // for entry in WalkDir::new("bar").into_iter().filter_map(|e| e.ok()) {
+    //     println!("{:?}", entry.metadata());
+    //     println!("{}", entry.path().display());
+    // }
+    // for entry in WalkDir::new("baz").into_iter().filter_map(|e| e.ok()) {
+    //     println!("{:?}", entry.metadata().unwrap().is_file());
+    //     println!("{}", entry.path().display());
+    // }
+    // let test_path = "/home/sschlinkert";
+```
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,16 @@
-// use std::fs::File;
-// use blake2::{Blake2b, Digest};
-
-use std::path::Path;
 use std::{fs, io};
 use walkdir::WalkDir;
 
 fn main() {
-    // let hash1 = blake3::hash(b"test");
-    // println!("Found hash: {}", hash1);
+    let test_path = "/media/sschlinkert/My Book/back-ups-rsync-snapshot-feb-2021/Documents/macbook-air-code/buzzfeed";
+    println!("path is {}", test_path);
 
-    // let path = "foo.txt";
-    // let hasher = blake3::Hasher::new();
-    // let hash1 = hash(&path, hasher.clone());
-    // // let mut hasher = blake3::Hasher::new();
-    // let hash2 = hash(&path, hasher.clone());
-    // println!("{}\nvs\n{}", hash1, hash2);
-
-    // for entry in WalkDir::new("bar").into_iter().filter_map(|e| e.ok()) {
-    //     println!("{:?}", entry.metadata());
-    //     println!("{}", entry.path().display());
-    // }
-    // for entry in WalkDir::new("baz").into_iter().filter_map(|e| e.ok()) {
-    //     println!("{:?}", entry.metadata().unwrap().is_file());
-    //     println!("{}", entry.path().display());
-    // }
-    let test_path = "/home/sschlinkert";
-    // let test_path = "/media/sschlinkert/My Book/back-ups-rsync-snapshot-feb-2021";
     println!("Making first hash");
     let hash_vec1 = hash_dir(test_path);
     println!("Making second hash");
     let hash_vec2 = hash_dir(test_path);
-    // let hash_vec2 = hash_dir("baz");
 
-    // println!("{:?}\nvs\n{:?}", hash_vec1, hash_vec2);
+    println!("{:?}\nvs\n{:?}", hash_vec1, hash_vec2);
     if hash_vec1 == hash_vec2 {
         println!("Matched!");
     } else {
@@ -40,42 +18,17 @@ fn main() {
     }
 }
 
-fn hash(path: &Path, mut hasher: blake3::Hasher) -> String {
-    let mut file = fs::File::open(&path).expect("Error opening a file for hashing");
-    // let mut hasher = Blake2b::new();
-    let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
-    let hash = hasher.finalize();
-    // println!("Path: {}", path);
-    // println!("Bytes processed: {}", n);
-    // println!("Hash value: {:?}", hash);
-    return hash.to_string();
-}
-
-fn hash_dir(dir_path: &str) -> Vec<String> {
-    let mut hash_dir = vec![];
-    let hasher = blake3::Hasher::new();
+fn hash_dir(dir_path: &str) -> blake3::Hash {
+    let mut hasher = blake3::Hasher::new();
 
     for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
         // println!("{:?}", entry.path());
-        if entry.metadata().unwrap().is_file() == true
-            && entry.path().starts_with("/home/sschlinkert/.bash_history") == false
-            && entry.path().starts_with("/home/sschlinkert/.bash_logout") == false
-            && entry.path().starts_with("/home/sschlinkert/.cargo") == false
-            && entry.path().starts_with("/home/sschlinkert/.cache") == false
-            && entry.path().starts_with("/home/sschlinkert/.config") == false
-            && entry.path().starts_with("/home/sschlinkert/.gtkrc-2.0") == false
-            && entry.path().starts_with("/home/sschlinkert/.gnupg") == false
-            && entry.path().starts_with("/home/sschlinkert/.Xauthority") == false
-            && entry
-                .path()
-                .starts_with("/home/sschlinkert/.xsession-errors")
-                == false
+        if entry.metadata().unwrap().is_file()
+            && entry.path().starts_with("/home/sschlinkert/.") == false
         {
-            // println!("{}", entry.path().display());
-            let this_file_hash = hash(entry.path(), hasher.clone());
-            // hash_dir.push(this_file_hash.to_string());
-            hash_dir.push(this_file_hash);
+            let mut file = fs::File::open(&entry.path()).expect("Error opening a file for hashing");
+            let _n = io::copy(&mut file, &mut hasher).expect("Error hashing a file");
         }
     }
-    hash_dir
+    hasher.finalize()
 }

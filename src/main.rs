387d27a9f8ec1fe9805a9deb5aa8d7af3a859dae
Sam Schlinkert <sschlinkert@gmail.com>
2021-11-02 21:50:19 -0400

    makes some tests more thorough

--- src/main.rs
+++ src/main.rs
@@ -221,16 +221,29 @@ mod basic_tests {
     use super::*;
 
     #[test]
-    fn can_determine_copied_directory_is_same_from_paths() {
+    fn can_determine_same_directory_is_same() {
         let test_path = Path::new("./test-files/bar");
         assert_eq!(hash_dir(&test_path, 1), hash_dir(&test_path, 1));
+        assert_eq!(hash_dir(&test_path, 2), hash_dir(&test_path, 2));
+        assert_eq!(hash_dir(&test_path, 4), hash_dir(&test_path, 4));
     }
 
     #[test]
     fn can_determine_different_directories_are_different() {
         let path1 = Path::new("/home/sschlinkert/code/tic-tac-go");
         let path2 = Path::new("/home/sschlinkert/code/tidy");
+        assert_ne!(hash_dir(&path1, 1), hash_dir(&path2, 1));
+        assert_ne!(hash_dir(&path1, 2), hash_dir(&path2, 2));
         assert_ne!(hash_dir(&path1, 4), hash_dir(&path2, 4));
+    }
+
+    #[test]
+    fn can_determine_copied_directory_is_same_from_paths_even_have_have_different_paths_and_path_lengths(
+    ) {
+        let test_path1 = Path::new("./test-files/bar");
+        let test_path2 = Path::new("./test-files/back-ups/bar");
+        assert_eq!(hash_dir(&test_path1, 2), hash_dir(&test_path2, 2));
+        assert_eq!(hash_dir(&test_path1, 4), hash_dir(&test_path2, 4));
     }
 
     #[test]
@@ -245,13 +258,6 @@ mod basic_tests {
         let test_path1 = Path::new("./test-files/bar");
         let test_path2 = Path::new("./test-files/baz");
         assert_eq!(hash_dir(&test_path1, 2), hash_dir(&test_path2, 2));
-    }
-
-    #[test]
-    fn can_determine_copied_directory_is_same_from_paths_even_have_have_different_paths_and_path_lengths(
-    ) {
-        let test_path1 = Path::new("./test-files/bar");
-        let test_path2 = Path::new("./test-files/back-ups/bar");
         assert_eq!(hash_dir(&test_path1, 4), hash_dir(&test_path2, 4));
     }
 
